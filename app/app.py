7b7ff8e91f3e4a23346876e531f7fe9ca97469f7
Tianxiang from Dell5510 <tsu@slb.com>
2019-12-28 23:46:39 -0500

    capable of showing questions. can have a login page

--- app/app.py
+++ app/app.py
@@ -1,21 +1,43 @@
 import mysql.connector
-
-config = {
-        'user': 'devuser',
-        'password': 'devpass',
-        'host': 'db',
-        'port': '3306',
-        'database': 'tianxiang_db'
-    }
-connection = mysql.connector.connect(**config)
-print('here')
-
 import flask
+from flask import Flask, request, render_template
+
 app = flask.Flask(__name__)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return "OK."
+
+
+
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    config = {
+            'user': 'devuser',
+            'password': 'devpass',
+            'host': 'db',
+            'port': '3306',
+            'database': 'tianxiang_db'
+        }
+    connection = mysql.connector.connect(**config)
+
+    sql_select_Query = "select * from vocabulary"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    #print("Total number of rows in Laptop is: ", cursor.rowcount)
+
+    return "Records:{}\nType:{}".format(records,records[0][1]) #format(cursor.rowcount) #'Flask Dockerized'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
